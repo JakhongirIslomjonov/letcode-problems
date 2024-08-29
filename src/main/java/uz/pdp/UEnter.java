b4b71a3d7142196494a4ca2a5bc85f0652b306a5
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-08-29 14:38:33 +0500

    Uenter homework

---
+++ src/main/java/uz/pdp/UEnter.java
@@ -0,0 +1,58 @@
+package uz.pdp;
+
+public class UEnter {
+    public static void main(String[] args) {
+        //1-
+       /* System.out.println("+________________________________+");
+        System.out.println("|Name   : Jakhongir Islomjonov    |");
+        System.out.println("|Email  : jahongirnamti@gmamil.com|");
+        System.out.println("|Address: Tashkent , Uzbekistan  |");
+        System.out.println("+--------------------------------+");
+*/
+//2-
+      /*  Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.next();
+        System.out.println("Hello "+name+" !");
+*/
+
+        //3-
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter rectangle width :");
+        int width = scanner.nextInt();
+        System.out.print("Please enter rectangle height :");
+        int height = scanner.nextInt();
+        System.out.println("Area : width * height = " + (width * height));
+        System.out.println("Perimeter: 2 * ( width + height ) = " + (2 * (width + height)));
+*/
+
+        //4-
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius  of a cylinder: ");
+        double radius = scanner.nextDouble();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter the length of a cylinder: ");
+        double length = scanner1.nextDouble();
+        double area = Math.pow(radius, 2) * Math.PI;
+        System.out.println("The area is area = radius * radius * Pi =" + area);
+        System.out.println("The volume is volume = area * length = " + (area * length));
+*/
+
+        //5-
+      /*  Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length of the sides of the Equilateral triangle:");
+        int side = scanner.nextInt();
+        System.out.print("Enter length of the height of the Equilateral triangle:");
+        int height = scanner.nextInt();
+        double area = (Math.pow(3, 0.5) / 4) * Math.pow(side, 2);
+        System.out.println("The area is area =\n" +
+                "√\n" +
+                "3\n" +
+                "4\n" +
+                "∗ side2\n = " + area);
+        System.out.println("The volume of the Triangular prism is volume = area * length = " + area *height);
+*/
+
+    }
+}
