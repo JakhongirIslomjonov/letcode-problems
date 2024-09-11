6c89242c1ed73b658556e37a4350f56b9897f88d
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-09-11 18:50:26 +0500

    U-enter homework

--- src/main/java/uz/pdp/GenericsExample.java
+++ src/main/java/uz/pdp/GenericsExample.java
@@ -1,8 +1,23 @@
 package uz.pdp;
 
+import java.util.ArrayList;
+
 public class GenericsExample {
     public static void main(String[] args) {
 
+
+    }
+
+    public static ArrayList<String> split(String s, String regex) {
+        ArrayList<String> result = new ArrayList<>();
+
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.substring(i, regex.length()).equals(regex)) {
+
+            }
+        }
+        return result  ;
     }
 
 }
