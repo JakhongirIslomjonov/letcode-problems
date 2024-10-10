ac9f00552c46e3af4f834747dbbf382db77d2d01
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-10-10 19:08:33 +0500

    leetcode solution

--- src/main/java/uz/pdp/leetcode/Second.java
+++ src/main/java/uz/pdp/leetcode/Second.java
@@ -1,5 +1,7 @@
 package uz.pdp.leetcode;
 
+import javax.swing.tree.TreeNode;
+
 public class Second {
     public static void main(String[] args) {
         System.out.println(searchInsert(new int[]{1, 2, 3, 4, 5, 6}, 2));
@@ -25,8 +27,38 @@ public class Second {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
             if (nums[mid] > target) right = mid - 1;
-            else left =mid +1;
+            else left = mid + 1;
         }
         return -1;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+        int sum = 0;
+        sum += (root.val >= low && root.val <= high) ? root.val : 0;
+        rangeSumBST(root.left, low, high);
+        rangeSumBST(root.right, low,high);
+    }
+    int rangeSumBST(TreeNode  root, int low, int hight){
+      e
     }
 }
